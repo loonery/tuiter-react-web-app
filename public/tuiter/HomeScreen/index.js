3c74b68dd695761237b653f3f2eed2b9173c7319
Ryan <ryanlooney58@gmail.com>
2022-10-14 13:17:39 -0400

    starting challenge portion of A5

---
+++ public/tuiter/HomeScreen/index.js
@@ -0,0 +1,24 @@
+import navigationSidebar from "../NavigationSidebar";
+
+const homeScreen = () => {
+    $('#wd-homeScreen').append(`
+        <div class="row mt-2"> 
+            <!-- Sidebar Component Loaded In -->
+            <div class="col-xl-2 col-lg-1 col-md-2 col-sm-2">
+                ${navigationSidebar()}
+            </div>
+            <!-- Main Content Component Loaded In -->
+            <div class="col-xl-6 col-lg-7 col-md-10 col-sm-10">
+
+            
+            </div>
+            <!-- Navigation Sidebar Component Loaded In -->
+            <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+
+
+
+            </div>
+        </div>
+    `);
+}
+$(homeScreen)
